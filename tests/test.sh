4a4289d4ff2a4e7b871ce2e83f4e4b5f83ef10e6
Liron Ganel <lganel@wustl.edu>
2016-06-03 17:37:09 -0500

    Updated testing code for vcfanno v0.0.11. test.sh now outputs results of stress test before beginning NA12878 test

--- tests/test.sh
+++ tests/test.sh
@@ -16,21 +16,12 @@ echo "Generating annotation files"
 cd ..
 ./svscore.pl -f tests/dummyannotations.introns.bed -e tests/dummyannotations.exons.bed -o max,sum,top2,top2weighted,top3weighted,top4weighted,mean,meanweighted -dvc tests/dummyCADD.tsv.gz -i tests/stresstest.vcf | grep -v "^#" > tests/stresstest.svscore.test.vcf
 cd tests
-echo -e "\n\n********NA12878********"
-echo "Generating annotation files"
-../generateannotations.pl
-cd ..
-./svscore.pl -o max,sum,top5,top10,mean -dvc $1 -i tests/NA12878.sv.vcf | grep -v "^#" > tests/NA12878.sv.svscore.test.vcf
 echo -e "\n\n"
-cd tests
 
 diff stresstest.svscore.test.vcf stresstest.svscore.noheader.vcf > stresstest.diff
-diff NA12878.sv.svscore.test.vcf NA12878.sv.svscore.noheader.vcf > NA12878.diff
 
 STRESSTESTOUTPUTLINES=`cat stresstest.svscore.test.vcf | wc -l`
 STRESSTESTDIFFLINES=`cat stresstest.diff | wc -l`
-NA12878OUTPUTLINES=`cat NA12878.sv.svscore.test.vcf | wc -l`
-NA12878DIFFLINES=`cat NA12878.diff | wc -l`
 
 if [ $STRESSTESTDIFFLINES = 0 ] && [ $STRESSTESTOUTPUTLINES -gt 0 ]; then
   echo "********STRESSTEST TEST PASSED********"
@@ -43,6 +34,20 @@ else
     echo "********STRESSTEST: NO OUTPUT********"
   fi
 fi
+
+# NA12878
+echo -e "********NA12878********"
+echo "Generating annotation files"
+../generateannotations.pl
+cd ..
+./svscore.pl -o max,sum,top5,top10,mean -dvc $1 -i tests/NA12878.sv.vcf | grep -v "^#" > tests/NA12878.sv.svscore.test.vcf
+cd tests
+echo -e "\n\n"
+
+diff NA12878.sv.svscore.test.vcf NA12878.sv.svscore.noheader.vcf > NA12878.diff
+
+NA12878OUTPUTLINES=`cat NA12878.sv.svscore.test.vcf | wc -l`
+NA12878DIFFLINES=`cat NA12878.diff | wc -l`
 
 if [ $NA12878DIFFLINES = 0 ] && [ $NA12878OUTPUTLINES -gt 0 ]; then
   echo "********NA12878 TEST PASSED********"
