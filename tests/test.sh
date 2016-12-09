7e0f54c684c82751c6f2bb7018d05658f0af3925
Liron Ganel <lganel@wustl.edu>
2016-12-09 16:44:13 -0600

    Make test.sh work from any directory

--- tests/test.sh
+++ tests/test.sh
@@ -14,9 +14,9 @@ echo "*            *"
 echo "**************"
 
 echo "Generating annotation files"
-$SVSCOREDIR/generateannotations.pl -c 1 -a 2 -b 3 -t 4 -s 5 -e 6 -f 7  -o tests tests/dummyannotations.bed
-$SVSCOREDIR/svscore.pl -f tests/dummyannotations.introns.bed -e tests/dummyannotations.exons.bed -o max,sum,top2,top2weighted,top3weighted,top4weighted,mean,meanweighted -dvc tests/dummyCADD.tsv.gz -i tests/stresstest.vcf > tests/stresstest.svscore.test.vcf
-perl tests/scorecomparison.pl STRESSTEST tests/stresstest.svscore.vcf tests/stresstest.svscore.test.vcf
+$SVSCOREDIR/generateannotations.pl -c 1 -a 2 -b 3 -t 4 -s 5 -e 6 -f 7  -o $SVSCOREDIR/tests $SVSCOREDIR/tests/dummyannotations.bed
+$SVSCOREDIR/svscore.pl -f $SVSCOREDIR/tests/dummyannotations.introns.bed -e $SVSCOREDIR/tests/dummyannotations.exons.bed -o max,sum,top2,top2weighted,top3weighted,top4weighted,mean,meanweighted -dvc $SVSCOREDIR/tests/dummyCADD.tsv.gz -i $SVSCOREDIR/tests/stresstest.vcf > $SVSCOREDIR/tests/stresstest.svscore.test.vcf
+perl $SVSCOREDIR/tests/scorecomparison.pl STRESSTEST $SVSCOREDIR/tests/stresstest.svscore.vcf $SVSCOREDIR/tests/stresstest.svscore.test.vcf
 echo
 echo
 
@@ -27,9 +27,9 @@ echo "* NA12878 *"
 echo "*         *"
 echo "***********"
 echo "Generating annotation files"
-$SVSCOREDIR/generateannotations.pl -o tests
-$SVSCOREDIR/svscore.pl -o max,sum,top5,top10,mean -e tests/refGene.exons.bed -f tests/refGene.introns.bed -dvc $1 -i tests/NA12878.sv.vcf > tests/NA12878.sv.svscore.test.vcf
-perl tests/scorecomparison.pl NA12878 tests/NA12878.sv.svscore.vcf tests/NA12878.sv.svscore.test.vcf
+$SVSCOREDIR/generateannotations.pl -o $SVSCOREDIR/tests
+$SVSCOREDIR/svscore.pl -o max,sum,top5,top10,mean -e $SVSCOREDIR/tests/refGene.exons.bed -f $SVSCOREDIR/tests/refGene.introns.bed -dvc $1 -i $SVSCOREDIR/tests/NA12878.sv.vcf > $SVSCOREDIR/tests/NA12878.sv.svscore.test.vcf
+perl $SVSCOREDIR/tests/scorecomparison.pl NA12878 $SVSCOREDIR/tests/NA12878.sv.svscore.vcf $SVSCOREDIR/tests/NA12878.sv.svscore.test.vcf
 
 rm -rf svscoretmp/
-rm -f tests/NA12878.sv.svscore.test.vcf tests/stresstest.svscore.test.vcf tests/dummyannotations.*.bed* tests/refGene* conf.toml
+rm -f $SVSCOREDIR/tests/NA12878.sv.svscore.test.vcf $SVSCOREDIR/tests/stresstest.svscore.test.vcf $SVSCOREDIR/tests/dummyannotations.*.bed* $SVSCOREDIR/tests/refGene* conf.toml
